763af488f9e1e6cc98e8a736b2f0e861e3f22b96
Stephen Burns <steve@exp.org.au>
2020-03-17 15:42:30 +1100

    initial commit

---
+++ Assets/Scripts/CubeInteractable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+public class CubeInteractable : MonoBehaviour, IInteractable
+{
+    public bool _Interacted = false;
+
+    public void OnRayHit()
+    {
+        _Interacted = true;
+        GetComponent<Renderer>().material.color = Color.red;
+    }
+
+}
+
---
+++ Assets/Scripts/InteractionHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+public class InteractionHandler : MonoBehaviour
+{
+    public KeyCode _InteractionKey;
+    public Transform _CamTransform;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(_InteractionKey))
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(_CamTransform.position, Vector3.forward, out hit, 100f))
+            {
+                IInteractable hitInteractable = hit.collider.gameObject.GetComponent<IInteractable>();
+                if(hitInteractable != null)
+                {
+                    hitInteractable.OnRayHit();
+                }
+            }
+        }
+    }
+}
+
